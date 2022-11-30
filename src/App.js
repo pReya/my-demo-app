78874d8723444d855d49e43367711e2081f478a5
Moritz Stückler <moritz@bitbetter.de>
2022-11-30 18:51:30 +0100

    feat: add custom text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello HCS Class of 2022!
         </a>
       </header>
     </div>
